2eec410d00752686fcc8e17dc748136c43d0fb85
MrErik402 <bujakierikattila@turr.hu>
2025-06-03 11:48:12 +0200

    Blog üdvözlö szöveg feltöltés.

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <div class="footer-img-overlay">
     <div class="footer-img-content">
       <div class="footer-box">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>A kenyér az élet alapja – nálunk minden falat szeretettel készül.</p>
       </div>
       <div class="footer-box">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
--- page-blog.php
+++ page-blog.php
@@ -10,7 +10,8 @@ get_header(); ?>
     <div class="blog-container">
         <div class="blog-info-box">
             <h2>BLOG</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p><strong>Üdvözlünk a Péksütemények világában!</strong>
+            Itt minden a friss, illatos, ropogós és puha finomságokról szól, amelyeket a pékség varázsol az asztalodra. Fedezd fel a legjobb recepteket, tippeket és történeteket, hogy otthon is igazi ínyencségeket készíthess! Csatlakozz hozzánk, és süssünk együtt boldogságot!</p>
         </div>
         <div class="blog-cards-grid">
             <div class="blog-card">
