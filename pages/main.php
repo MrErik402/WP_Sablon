71abe076832135eec958aed1631c5258769c854c
Dóczi Adrián <98951852+docziadrian@users.noreply.github.com>
2025-06-03 11:10:12 +0200

    php fájlok feltöltése

---
+++ pages/main.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Main Page
+ * 
+ * The template for displaying the main landing page
+ */
+
+get_header(); ?>
+
+<div class="main-page-container" style="background-image: url('<?php echo get_template_directory_uri(); ?>/Pictures/fooldal.png'); background-size: cover; background-position: center; min-height: 100vh;">
+    <div class="main-content">
+        <h1>asd</h1>
+    </div>
+</div>
+
+<?php get_footer(); ?>
