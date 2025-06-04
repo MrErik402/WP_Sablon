10874fa9214f2f24331b3d3cea813dae5552f8d3
MrErik402 <bujakierikattila@turr.hu>
2025-06-04 10:06:54 +0200

    Blog oldal szöveg beillesztés

--- page-blog.php
+++ page-blog.php
@@ -10,7 +10,11 @@ get_header(); ?>
     <div class="blog-container">
         <div class="blog-info-box">
             <h2>BLOG</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p><strong>Üdvözlünk szeretettel blogunkban!</strong>
+            <br>
+            Öröm számunkra, hogy idelátogattál – foglalj helyet, lazíts, és merülj el történeteinkben, ízekben, ötletekben és inspirációkban! Legyen szó konyhai titkokról, éttermi kulisszatitkokról, friss receptekről vagy éppen élet apró örömeiről, itt mindig találsz egy csipetnyi varázst. Kívánjuk, hogy minden bejegyzésünk hozzon egy kis mosolyt, gondolatot vagy épp ízélményt a napodba!
+
+</p>
         </div>
         <div class="blog-cards-grid">
             <?php
